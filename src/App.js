95456895d846fe405de1d4f08ac0329e5539d495
aviralgarg0996 <aviralgarg0996@gmail.com>
2020-09-14 10:01:37 +0530

    Counter test cases

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-class App extends React.Component{
-  render(){
-    return(
-      <div data-test=''></div>
-    )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">
+          The counter is currently {this.state.counter}
+        </h1>
+        <button
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+          data-test="increment-button"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            if (this.state.counter > 0)
+              this.setState({ counter: this.state.counter - 1 });
+          }}
+          data-test="decrement-button"
+        >
+          Decrement
+        </button>
+      </div>
+    );
   }
 }
 
