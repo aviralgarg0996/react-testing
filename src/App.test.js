973a47d1161d7e74c3d4dea8d119edc99ba16741
aviral-fa <aviral.garg@bakingo.com>
2020-09-14 08:26:32 +0530

    master

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import Enzyme from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+// test("renders learn react link", () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+test("renders without error", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test=component-app");
+  expect(appComponent.length).toBe(1);
 });
+test("renders increment button", () => {});
+test("renders counter display", () => {});
+test("renders counter starts at 0", () => {});
+test("renders clicking button increments counter display", () => {});
